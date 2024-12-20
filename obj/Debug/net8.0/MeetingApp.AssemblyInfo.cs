ccb27a82d86efa30cc142b4893e31a743cc0b0a8
emrahaltuner <nanoxtanitim@gmail.com>
2024-12-20 20:46:07 +0300

    Update assembly informational version and cache inputs

--- obj/Debug/net8.0/MeetingApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MeetingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6778c4d36ac3ef6e010b29da1fbe2ea75005ed3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63fd44cdf8b54a2d7cb056f9de10ccb7c6bafb8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
